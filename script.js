404f72d78be456bce8d4ce18fc11b7ea5b8cc7f2
Julia Miskey <juliamiskey@gmail.com>
2018-05-02 17:20:59 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,23 @@
+
+// User is presented with 2 options (radio buttons)
+
+// User clicks one of the two options 
+
+// Once ok with their answer user clicks submit
+
+// Upon clicking submit a pop-up or another div appears:
+    // If chosen option has a class of "answer", the correct answer window appears and says YAY
+    // If chosen option does not have a class of "answer", the incorrect answer window appears and says BOO
+    
+    if (answer === true) {
+    $('.option').addClass('correct'); ??
+}
+
+// The page now displays the next question
+
+// User continues to next question and clicks on one of the two options
+
+// BONUS
+// COUNTER - FINAL SCORE
+// if less than 50% correct "You are living in an alternate reality"
+// else "you have a great memory and bragging rights!""
